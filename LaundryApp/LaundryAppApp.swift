5199162d285b62d9e1de5d09bf6554f49fc3d5cb
GrantHough <89223313+GrantHough@users.noreply.github.com>
2022-01-13 21:34:16 -0800

    Initial Commit

---
+++ LaundryApp/LaundryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaundryAppApp.swift
+//  LaundryApp
+//
+//  Created by Grant Hough on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct LaundryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
