0ca48ecf75ee878d250d8893289811730e7bd45a
GrantHough <89223313+GrantHough@users.noreply.github.com>
2022-01-15 00:19:15 -0800

    sdfsdf
    \/sdfsdf

---
+++ LaundryApp/SignIn.swift
@@ -0,0 +1,20 @@
+//
+//  SignIn.swift
+//  LaundryApp
+//
+//  Created by Grant Hough on 1/15/22.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignIn_Previews: PreviewProvider {
+    static var previews: some View {
+        SignIn()
+    }
+}
---
+++ LaundryApp/SignUp.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp.swift
+//  LaundryApp
+//
+//  Created by Grant Hough on 1/15/22.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
