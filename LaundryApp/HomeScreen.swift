365370d374523f1cd762b564b2a187d66b282275
GrantHough <89223313+GrantHough@users.noreply.github.com>
2022-01-15 01:12:14 -0800

    odifugoiufoirwredf
    sdfsdfsfdsfs

--- LaundryApp/HomeScreen.swift
+++ LaundryApp/HomeScreen.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct HomeScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    
+    @State var x: [LaundryHouse] = [ LaundryHouse(name: "Joan Singh", address: "6969 420 Streat WeedLansd USA"), LaundryHouse(name: "weedDealer", address: "I like doing weed at 1234 alphabet soup lane"), LaundryHouse(name: "Grant Hough", address: "14803 Granite Way"), LaundryHouse(name: "Neal Malhotra", address: "13030 La Vista Drive")]
+
+        var body: some View {
+            Text("")
+            List(x, id: .name) { xx in
+                VStack{
+                    Text(xx.name)
+                    Text(xx.address)
+                }
+
+            }
+        }
 }
+
+
 
 struct HomeScreen_Previews: PreviewProvider {
     static var previews: some View {
---
+++ LaundryApp/LaundryHouse.swift
@@ -0,0 +1,21 @@
+//
+//  LaundryHouse.swift
+//  LaundryApp
+//
+//  Created by Grant Hough on 1/15/22.
+//
+
+import Foundation
+
+struct LaundryHouse {
+    
+    var name: String = ""
+    var address: String = ""
+
+    init(name: String, address: String){
+        self.name = name
+        self.address = address
+
+    }
+
+}
