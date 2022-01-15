f5946e38210d860c8ee4f72468c3290e5a8d2cdf
GrantHough <89223313+GrantHough@users.noreply.github.com>
2022-01-14 20:03:10 -0800

    Beginning to Work
    I believe I made the Github Repo yesterday but I didn't work on it. Committing to start the Hackathon.

--- LaundryApp/ContentView.swift
+++ LaundryApp/ContentView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Title: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Title_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Title()
     }
 }
