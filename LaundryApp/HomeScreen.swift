ee7081bbbd945c636dfe283c8448fc9adb9e6a31
Neal Malhotra <nealmalhotra2006@gmail.com>
2022-01-15 01:19:16 -0800

    hi grant

--- LaundryApp/HomeScreen.swift
+++ LaundryApp/HomeScreen.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeScreen: View {
     
     @State var x: [LaundryHouse] = [ LaundryHouse(name: "Joan Singh", address: "6969 420 Streat WeedLansd USA"), LaundryHouse(name: "weedDealer", address: "I like doing weed at 1234 alphabet soup lane"), LaundryHouse(name: "Grant Hough", address: "14803 Granite Way"), LaundryHouse(name: "Neal Malhotra", address: "13030 La Vista Drive")]
-
+    //hi grant
         var body: some View {
             Text("")
             List(x, id: \.name) { xx in
